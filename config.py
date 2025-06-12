c56bc52a8428817d0272160cf07c5f912bd0142a
SebastiaanOsinga <56591341+SebastiaanOsinga@users.noreply.github.com>
2025-06-12 11:27:29 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+nyuu_config = {
+    'username': 'username',
+    'password': 'password',
+    'from': 'from@adress.com',
+    'host': 'usenethost',
+    'port': 'portused',
+    'connections': 'amount of conenctions as INT',
+    'newsgroup': 'alt.binaries.test' #change to alt.binarie adres.
+}
